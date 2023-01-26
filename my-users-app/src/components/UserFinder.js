26ed91df167d966bc934fca3e5648736c846f832
LucasJann <lucas.jan.jobs@gmail.com>
2023-01-26 00:55:59 -0300

    Adding UserFinder component and some context to our learning

---
+++ my-users-app/src/components/UserFinder.js
@@ -0,0 +1,76 @@
+import { Component, Fragment, useState, useEffect } from "react";
+
+import Users from "./Users";
+import classes from './UserFinder.module.css';
+import UsersContext from "../../../store/user-context";
+
+const DUMMY_USERS = [
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
+];
+
+class UserFinder extends Component {
+  static contextType = UsersContext;
+  constructor() {
+    super()
+    this.state = {
+      filteredUsers: [DUMMY_USERS],
+      searchTerm: "",
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: DUMMY_USERS.filter((user) =>
+          user.name.includes(this.state.searchTerm)
+        ),
+      });
+    }
+  }
+
+  componentDidMount() {
+    this.setState({filteredUsers: this.context.users})
+  }
+
+  searchChangeHandler(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <UsersContext.Consumer>
+
+        </UsersContext.Consumer>
+        <input className={classes.finder} type="search" onChange={this.searchChangeHandler.bind(this)} />
+        <Users users={this.state.filteredUsers} />
+      </Fragment>
+    );
+  }
+}
+
+// const UserFinder = () => {
+//   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
+//   const [searchTerm, setSearchTerm] = useState("");
+
+//   useEffect(() => {
+//     setFilteredUsers(
+//       DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
+//     );
+//   }, [searchTerm]);
+
+//   const searchChangeHandler = (event) => {
+//     setSearchTerm(event.target.value);
+//   };
+
+//   return (
+//     <Fragment>
+//       <input type="search" onChange={searchChangeHandler} />
+//       <Users users={filteredUsers} />
+//     </Fragment>
+//   );
+// };
+
+export default UserFinder;
