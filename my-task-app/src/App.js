7e794246fe0dada763992eaeeb5462fd8d8558fa
LucasJann <lucas.jan.jobs@gmail.com>
2023-01-28 16:24:07 -0300

    In this project we did work with fetching GET and POST, HTTP Request, and how to work with Firebase

---
+++ my-task-app/src/App.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./components/Hooks/use-http";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const transformTasks = (tasksObj) => {
+    const loadedTasks = [];
+
+    for (const taskKey in tasksObj) {
+      console.log(tasksObj)
+      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+    }
+
+    setTasks(loadedTasks);
+  };
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  useEffect(() => {
+    fetchTasks({
+      url: "https://react-http-f8211-default-rtdb.firebaseio.com/tasks.json",
+    }, transformTasks);
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
