e4895d0f73837fe6d4cca5d7bff95af0b82d8a1f
LucasJann <lucas.jan.jobs@gmail.com>
2023-06-12 18:00:49 -0300

    The code has been organized

--- my-authenticated-router-project-app/frontend/src/App.js
+++ my-authenticated-router-project-app/frontend/src/App.js
@@ -1,23 +1,29 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import EditEventPage from "./pages/EditEvent";
+import HomePage from "./pages/Home";
 import ErrorPage from "./pages/Error";
+import RootLayout from "./pages/Root";
+import NewEventPage from "./pages/NewEvent";
+import EditEventPage from "./pages/EditEvent";
+import EventsRootLayout from "./pages/EventsRoot";
+
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
+import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
+
+import { checkAuthLoader, tokenLoader } from "./util/auth";
+import { action as logoutAction } from "./pages/Logout";
+import { action as manipulateEventAction } from "./components/EventForm";
+
+import AuthenticationPage, {
+  action as authAction,
+} from "./pages/Authentication";
+
 import EventDetailPage, {
   loader as eventDetailLoader,
   action as deleteEventAction,
 } from "./pages/EventDetail";
-import EventsPage, { loader as eventsLoader } from "./pages/Events";
-import EventsRootLayout from "./pages/EventsRoot";
-import HomePage from "./pages/Home";
-import NewEventPage from "./pages/NewEvent";
-import RootLayout from "./pages/Root";
-import { action as manipulateEventAction } from "./components/EventForm";
-import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
-import AuthenticationPage, {
-  action as authAction,
-} from "./pages/Authentication";
-import { action as logoutAction } from "./pages/Logout";
-import { checkAuthLoader, tokenLoader } from "./util/auth";
+
+
 
 const router = createBrowserRouter([
   {
