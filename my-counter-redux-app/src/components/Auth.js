507b4e4b6438fff87624ff7d48c3a90f6d61c1e0
LucasJann <lucas.jan.jobs@gmail.com>
2023-06-22 19:48:22 -0300

    I've implemented a simple authentication to the app, the button's CSS properties were also tweaked

--- my-counter-redux-app/src/components/Auth.js
+++ my-counter-redux-app/src/components/Auth.js
@@ -1,35 +1,73 @@
-import { useDispatch } from 'react-redux';
-import classes from './Auth.module.css';
-import { authActions } from '../store/auth';
+import React, { useRef, useState, useEffect } from "react";
 
+import classes from "./Auth.module.css";
+import { useDispatch } from "react-redux";
+import { authActions } from "../store/auth";
 
 const Auth = () => {
   const dispatch = useDispatch();
 
+  const email = useRef();
+  const password = useRef();
+
+  const [isBlockActive, setIsBlockActive] = useState(true);
+
+  const [isValidEmail, setIsValidEmail] = useState(false);
+  const [isValidPassword, setIsValidPassword] = useState(false);
+
+  const isActive = isValidEmail && isValidPassword ? true : false;
+
+  useEffect(() => {
+    isActive ? setIsBlockActive(false) : setIsBlockActive(true);
+  }, [isActive]);
+
+  const onEmailChangeHandler = () => {
+    email.current.value !== "" ? setIsValidEmail(true) : setIsValidEmail(false);
+  };
+
+  const onPasswordChangeHandler = () => {
+    password.current.value !== ""
+      ? setIsValidPassword(true)
+      : setIsValidPassword(false);
+  };
+
   const loginHandler = (event) => {
     event.preventDefault();
 
-    dispatch(authActions.login())
-  }
+    dispatch(authActions.login());
+  };
+
+  const buttonStyle = isBlockActive ? classes.invalid : classes.valid;
 
   return (
     <main className={classes.auth}>
       <section>
         <form onSubmit={loginHandler}>
           <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              ref={email}
+              onChange={onEmailChangeHandler}
+            />
           </div>
           <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              ref={password}
+              onChange={onPasswordChangeHandler}
+            />
           </div>
-          <button>Login</button>
+          <button disabled={isBlockActive} className={buttonStyle}>
+            Login
+          </button>
         </form>
       </section>
     </main>
   );
 };
-
 
 export default Auth;
